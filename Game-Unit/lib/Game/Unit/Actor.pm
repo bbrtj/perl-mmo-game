5e204ffa7d44c58b2a6d87305a95c83e97a4da05
brtastic <brtastic.dev@gmail.com>
2020-12-20 20:38:49 +0100

    Unit implementation

--- Game-Unit/lib/Game/Unit/Actor.pm
+++ Game-Unit/lib/Game/Unit/Actor.pm
@@ -2,9 +2,41 @@ package Game::Unit::Actor;
 
 use header;
 use Moo;
+use Game::Model::Player;
+use Game::Model::Character;
+use Game::Model::CharacterVariables;
+use Game::Types qw(Maybe InstanceOf HashRef);
 
 no header;
 
 with 'Game::Unit';
+
+has 'player' => (
+	is => 'rw',
+	isa => Maybe [InstanceOf ['Game::Model::Player']],
+	predicate => is_player,
+);
+
+has 'npc' => (
+	is => 'rw',
+
+	# isa => Maybe[InstanceOf['Game::Model::Npc']],
+	predicate => is_npc,
+);
+
+has 'character' => (
+	is => 'rw',
+	isa => InstanceOf ['Game::Model::Character'],
+);
+
+has 'variables' => (
+	is => 'rw',
+	isa => InstanceOf ['Game::Model::CharacterVariables'],
+);
+
+has 'precalculated' => (
+	is => 'rw',
+	isa => HashRef,
+);
 
 1;
--- Game-Unit/lib/Game/Unit/Battle.pm
+++ Game-Unit/lib/Game/Unit/Battle.pm
@@ -2,10 +2,23 @@ package Game::Unit::Battle;
 
 use header;
 use Moo;
+use Game::Model::Battle;
+use Game::Model::BattleContestant;
+use Game::Types qw(InstanceOf ArrayRef Tuple);
 
 no header;
 
 with 'Game::Unit';
+
+has 'battle' => (
+	is => 'rw',
+	isa => InstanceOf ['Game::Model::Battle'],
+);
+
+has 'contestants' => (
+	is => 'rw',
+	isa => ArrayRef [Tuple [InstanceOf ['Game::Model::BattleContestant'], InstanceOf ['Game::Unit::Actor']]],
+);
 
 1;
 
