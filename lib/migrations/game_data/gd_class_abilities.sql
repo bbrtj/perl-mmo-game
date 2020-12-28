446cf2b164b6d75d7c91d7057f7ce9469b9ba316
brtastic <brtastic.dev@gmail.com>
2020-12-28 21:25:46 +0100

    Remove two classes

--- lib/migrations/game_data/gd_class_abilities.sql
+++ lib/migrations/game_data/gd_class_abilities.sql
@@ -1,10 +1,8 @@
-INSERT INTO gd_class_abilities (class_id,ability_id) VALUES 
+INSERT INTO gd_class_abilities (class_id,ability_id) VALUES
 ('CLS_SAG','ABI_STRIKE')
 ,('CLS_WAR','ABI_STRIKE')
 ,('CLS_ROG','ABI_STRIKE')
 ,('CLS_BAR','ABI_STRIKE')
 ,('CLS_PRI','ABI_STRIKE')
 ,('CLS_PAT','ABI_STRIKE')
-,('CLS_ZEA','ABI_STRIKE')
-,('CLS_WIT','ABI_STRIKE')
 ;
--- lib/migrations/game_data/gd_lore_names.sql
+++ lib/migrations/game_data/gd_lore_names.sql
@@ -1,16 +1,14 @@
-INSERT INTO gd_lore_names (lore_id,language_id,name) VALUES 
+INSERT INTO gd_lore_names (lore_id,language_id,name) VALUES
 ('CLS_SAG','PL','Mędrzec')
 ,('CLS_WAR','PL','Wojownik')
 ,('CLS_BAR','PL','Bard')
 ,('CLS_ROG','PL','Łotrzyk')
 ,('CLS_PRI','PL','Kapłan')
 ,('CLS_PAT','PL','Tropiciel')
-,('CLS_ZEA','PL','Fanatyk')
-,('CLS_WIT','PL','Łowca czarownic')
 ,('LOC_GMDUNG','PL','Loch mistrzów gry')
 ,('RAT_WIL','PL','Siła woli')
 ;
-INSERT INTO gd_lore_names (lore_id,language_id,name) VALUES 
+INSERT INTO gd_lore_names (lore_id,language_id,name) VALUES
 ('RAT_CRC','PL','Szansa na uderzenie krytyczne')
 ,('RAT_CRD','PL','Obrazenia krytyczne')
 ,('RAT_MCRC','PL','Szansa na uderzenie krytyczne z magii')
@@ -22,7 +20,7 @@ INSERT INTO gd_lore_names (lore_id,language_id,name) VALUES
 ,('STT_INT','PL','Inteligencja')
 ,('STT_CHA','PL','Charyzma')
 ;
-INSERT INTO gd_lore_names (lore_id,language_id,name) VALUES 
+INSERT INTO gd_lore_names (lore_id,language_id,name) VALUES
 ('STT_STA','PL','Wytrzymałość')
 ,('STT_TEN','PL','Upór')
 ,('STT_INI','PL','Inicjatywa')
