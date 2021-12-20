4bb729ee428e085dd429495b8e57a24a02182b33
bbrtj <brtastic.dev@gmail.com>
2021-12-20 20:37:00 +0100

    General: Fix bugged translation of form errors

--- lib/Game/Form.pm
+++ lib/Game/Form.pm
@@ -5,7 +5,7 @@ use Form::Tiny -consistent;
 use header;
 
 form_hook after_error => sub ($self, $error) {
-	if (!$error->error isa i18n::) {
+	if (!($error->error isa i18n::)) {
 		$error->set_error(_tt $error->error);
 	}
 };
