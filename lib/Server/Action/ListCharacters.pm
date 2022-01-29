84d391ff2b04a4539cf68d400770b7500357fe96
bbrtj <brtastic.dev@gmail.com>
2022-01-29 23:18:34 +0100

    Perl: Use redis session to get characters for an user

--- lib/Server/Action/ListCharacters.pm
+++ lib/Server/Action/ListCharacters.pm
@@ -13,8 +13,9 @@ use constant name => 'list_characters';
 sub handle ($self, $session_id, $id, $data)
 {
 	state $repo = DI->get('units');
-	# TODO: get user from redis
-	my $unit = $repo->get_user($id);
+
+	my $session = $self->cache->load(PlayerSession => $session_id);
+	my $unit = $repo->get_user($session->user_id);
 
 	$self->send_to($session_id, Resource::CharacterList->new($unit, n => $id));
 };
