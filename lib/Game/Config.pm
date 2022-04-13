2520a7085ce7e6e023c674e199b8ad55a99429ca
bbrtj <brtastic.dev@gmail.com>
2022-04-13 17:44:26 +0200

    Perl: Replace constant configs in Game::Config with subs

--- lib/Game/Config.pm
+++ lib/Game/Config.pm
@@ -3,8 +3,14 @@ package Game::Config;
 use header;
 
 # required for the define DSL to work
-use constant define => {
-};
+sub define
+{
+	state $defined = {};
+	return $defined;
+}
 
-use constant config => define;
+sub config
+{
+	return define;
+}
 
