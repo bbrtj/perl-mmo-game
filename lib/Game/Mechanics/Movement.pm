e638fbac94d4ddc5a639bade151e6544e34a1125
bbrtj <bbrtj.pro@gmail.com>
2022-12-07 07:20:18 +0100

    Backend: fix deep recursion in movement mechanics routine

--- lib/Game/Mechanics/Movement.pm
+++ lib/Game/Mechanics/Movement.pm
@@ -16,10 +16,9 @@ sub move ($self, $movement, $elapsed, $map)
 	my $new_x = $variables->pos_x + $coeffs->[0] * $distance;
 	my $new_y = $variables->pos_y + $coeffs->[1] * $distance;
 
-	if (!$map->check_can_be_accessed($new_x, $new_y)) {
+	if (!$map->check_can_be_accessed($new_x, $new_y) && $movement->time < $elapsed) {
 		# try to find position closer to the wall / ledge. It's okay to be slow and precise here
-		# TODO: deep recursion??
-		# $self->move($movement, $elapsed - Game::Config->config->{map_precision} / $movement->speed, $map);
+		$self->move($movement, $elapsed - Game::Config->config->{map_precision} / $movement->speed, $map);
 		return !!0;
 	}
 
