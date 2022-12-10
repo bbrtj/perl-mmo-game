e23fd1e7b9e0bdd4eaf7874bd2fc122cf23c21b1
bbrtj <bbrtj.pro@gmail.com>
2022-12-10 22:39:32 +0100

    Backend: Create Server::Session to handle communication

--- lib/Server.pm
+++ lib/Server.pm
@@ -4,8 +4,7 @@ use My::Moose;
 use Mojo::IOLoop;
 use Server::Config;
 use Server::Worker;
-
-use all 'Model', 'X';
+use Server::Session;
 
 use header;
 
@@ -33,112 +32,21 @@ with qw(
 	Server::Role::Listening
 );
 
-# NOTE: this function needs to do the bare minimum to ensure low latency
-sub handle_message ($self, $session, $req_id, $type, $data = undef)
-{
-	X::Network::CorruptedInput->throw('no id or no type')
-		if !$req_id || !$type;
-
-	# $action may be either a normal or ingame action
-	# (both are really the same thing but differ in where they should be passed)
-	my $action = $self->worker->get_action($type);
-
-	X::Network::InvalidAction->throw("Got $type")
-		unless defined $action;
-
-	X::Network::InvalidState->throw(sprintf "Currently %s, needs %s", $session->state, $action->required_state)
-		unless $session->state eq $action->required_state;
-
-	# validate may return an object that was created from $data
-	try {
-		$data = $action->validate($action->deserializes && $data ? __deserialize($data) : $data);
-	}
-	catch ($e) {
-		X::Network::CorruptedInput->throw("$e");
-	}
-
-	$self->worker->data_bus->emit($action, $session, ($req_id, $data));
-
-	return;
-}
-
-sub connection ($self, $loop, $stream, $id)
+sub connection ($self, $stream, $id)
 {
 	$self->log->debug('New TCP connection from ' . $stream->handle->peerhost)
 		if Server::Config::DEBUG;
 
 	# TODO: check if a player session exists, if yes then hook onto it
 	# However, make sure we don't have two clients connected at once
-	my $session = Model::PlayerSession->new;
-	$self->cache_repo->save($session);
-
-	my $handle_feedback = sub ($data_href) {
-		my %data = $data_href->%*;
-
-		if (defined $data{echo}) {
-			$stream->write(
-				join(
-					Server::Config::PROTOCOL_CONTROL_CHARACTER,
-					($data{id} // ''),
-					($data{echo_type} // ''),
-					(is_ref $data{echo} ? __serialize($data{echo}) : $data{echo}),
-				)
-
-				# NOTE: this newline is essential for the client to get this data
-				. "\n",
-			);
-		}
-
-		if ($data{refresh}) {
-			$session = $self->cache_repo->load(PlayerSession => $session->id);
-		}
-	};
-
-	# react to tcp messages
-	# TODO: should exceptions be caught?
-	$stream->on(
-		read => sub ($, $bytes) {
-			chomp $bytes;
-
-			if ($bytes eq 'ping') {
-				$stream->write('ping');
-				return;
-			}
-
-			$self->log->debug("TCP message: '$bytes'")
-				if Server::Config::DEBUG;
-
-			# check the length of $bytes to avoid getting attacked
-			X::Network::CorruptedInput->throw
-				if length $bytes > Server::Config::PROTOCOL_MAX_LENGTH;
-
-			$self->handle_message(
-				$session,
-				split Server::Config::PROTOCOL_CONTROL_CHARACTER, $bytes, 3
-			);
-		}
-	);
-
-	$self->connections->{$id} = $handle_feedback;
-	my $cb = $self->channel_service->listen($session->id, $handle_feedback);
-
-	$stream->on(
-		close => sub {
-
-			# TODO: log out from the world
-			$self->channel_service->unlisten($session->id, $cb);
+	$self->connections->{$id} = Server::Session->new(
+		server => $self,
+		stream => $stream,
+		on_dropped => sub {
 			delete $self->connections->{$id};
-			$self->cache_repo->remove($session);
 		}
 	);
 
-	$stream->on(
-		error => sub ($, $err) {
-			$self->log->error("TCP Error: $err");
-		}
-	);
-
-	$stream->timeout(Server::Config::GAME_SERVER_TIMEOUT);
 	return;
 }
 
@@ -150,8 +58,8 @@ sub start ($self)
 		$self->channel_service,
 		undef,
 		sub {
-			foreach my $connection_cb (values $self->connections->%*) {
-				$connection_cb->(@_);
+			foreach my $session (values $self->connections->%*) {
+				$session->handle_feedback(@_);
 			}
 		}
 	);
@@ -162,9 +70,8 @@ sub start ($self)
 			reuse => 1,
 
 			# TODO: tls
-		} => sub {
-			unshift @_, $self;
-			goto \&connection;
+		} => sub ($, $stream, $id) {
+			$self->connection($stream, $id);
 		}
 	);
 
---
+++ lib/Server/Session.pm
@@ -0,0 +1,147 @@
+package Server::Session;
+
+use My::Moose;
+use Server::Config;
+
+use all 'X', 'Model';
+
+use header;
+
+has param 'server' => (
+	isa => Types::InstanceOf['Server'],
+	'handles->' => {
+		'cache_repo' => 'cache_repo',
+		'channel_service' => 'channel_service',
+		'worker' => 'worker',
+		'log' => 'log',
+	}
+);
+
+has param 'stream' => (
+	'handles->' => {
+		'send' => 'write',
+	},
+);
+
+has param 'on_dropped' => (
+	isa => Types::CodeRef,
+);
+
+has field 'session' => (
+	writer => 1,
+	default => sub {
+		Model::PlayerSession->new
+	},
+);
+
+with qw(
+	Server::Role::Listening
+);
+
+sub BUILD ($self, $)
+{
+	my $session = $self->session;
+	$self->cache_repo->save($session);
+	$self->_listen($self->channel_service, $session->id, sub {
+		$self->handle_feedback(@_);
+	});
+
+	# react to tcp events
+	my $stream = $self->stream;
+	$stream->on(read => sub ($, $bytes) { $self->unpack_message($bytes) });
+	$stream->on(close => sub { $self->dropped });
+	$stream->on(error => sub ($, $err) { $self->log->error("TCP Error: $err") });
+	$stream->timeout(Server::Config::GAME_SERVER_TIMEOUT);
+
+	# TODO: should exceptions be caught?
+
+	return;
+}
+
+sub dropped ($self)
+{
+	$self->cache_repo->remove($self->session);
+	$self->_unlisten;
+
+	# TODO: log out from the world
+
+	$self->on_dropped->();
+	return;
+}
+
+sub unpack_message ($self, $bytes)
+{
+	chomp $bytes;
+
+	if ($bytes eq 'ping') {
+		$self->send('ping');
+		return;
+	}
+
+	$self->log->debug("TCP message: '$bytes'")
+		if Server::Config::DEBUG;
+
+	# check the length of $bytes to avoid getting attacked
+	X::Network::CorruptedInput->throw
+		if length $bytes > Server::Config::PROTOCOL_MAX_LENGTH;
+
+	$self->handle_message(
+		split Server::Config::PROTOCOL_CONTROL_CHARACTER, $bytes, 3
+	);
+
+	return;
+}
+
+# NOTE: this function needs to do the bare minimum to ensure low latency
+sub handle_message ($self, $req_id, $type, $data = undef)
+{
+	X::Network::CorruptedInput->throw('no id or no type')
+		if !$req_id || !$type;
+
+	# $action may be either a normal or ingame action
+	# (both are really the same thing but differ in where they should be passed)
+	my $action = $self->worker->get_action($type);
+
+	X::Network::InvalidAction->throw("Got $type")
+		unless defined $action;
+
+	X::Network::InvalidState->throw(sprintf "Currently %s, needs %s", $self->session->state, $action->required_state)
+		unless $self->session->state eq $action->required_state;
+
+	# validate may return an object that was created from $data
+	try {
+		$data = $action->validate($action->deserializes && $data ? __deserialize($data) : $data);
+	}
+	catch ($e) {
+		X::Network::CorruptedInput->throw("$e");
+	}
+
+	$self->worker->data_bus->emit($action, $self->session, ($req_id, $data));
+
+	return;
+}
+
+sub handle_feedback ($self, $data_href)
+{
+	my %data = $data_href->%*;
+
+	if (defined $data{echo}) {
+		$self->send(
+			join(
+				Server::Config::PROTOCOL_CONTROL_CHARACTER,
+				($data{id} // ''),
+				($data{echo_type} // ''),
+				(is_ref $data{echo} ? __serialize($data{echo}) : $data{echo}),
+			)
+
+			# NOTE: this newline is essential for the client to get this data
+			. "\n",
+		);
+	}
+
+	$self->set_session($self->cache_repo->load(PlayerSession => $self->session->id))
+		if $data{refresh};
+
+	return;
+}
+
