5acd50ca2441d4531bb445d93e1bcd10e78679bc
bbrtj <bbrtj.pro@gmail.com>
2023-05-15 22:24:24 +0200

    Assets: create a basic water animation

--- assets/game tiles.tsx
+++ assets/game tiles.tsx
@@ -1,4 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.6" name="game tiles" tilewidth="100" tileheight="100" tilecount="400" columns="20">
  <image source="../client/data/images/terrain_tiles.png" width="2000" height="2000"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="500"/>
+   <frame tileid="1" duration="500"/>
+   <frame tileid="2" duration="500"/>
+   <frame tileid="3" duration="500"/>
+   <frame tileid="4" duration="500"/>
+   <frame tileid="3" duration="500"/>
+   <frame tileid="2" duration="500"/>
+   <frame tileid="1" duration="500"/>
+  </animation>
+ </tile>
 </tileset>
+
