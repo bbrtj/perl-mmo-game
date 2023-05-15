ea6ee6be0e68f67c49ca472de7ce91681e79ff3d
bbrtj <bbrtj.pro@gmail.com>
2023-05-15 21:55:03 +0200

    General: do not track tilesets (remove existing)

--- client/data/game tiles.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="game tiles" tilewidth="100" tileheight="100" tilecount="400" columns="20">
- <image source="images/terrain_tiles.png" width="2000" height="2000"/>
-</tileset>
-
